cd28d90c76d661901a568fba515f2738ad07450a
Voshiii <voshonfredison@gmail.com>
2025-09-17 16:04:47 +0200

    [Added] Login and signup buttons

--- lib/components/login_page_comp/my_button.dart
+++ lib/components/login_page_comp/my_button.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
+// ! TODO rename this widget?
 class MyButton extends StatelessWidget {
   final String text;
   final void Function()? onTap;
+  final Color color;
+  final bool showShadow;
 
 
   const MyButton({
     super.key,
     required this.text,
     required this.onTap,
-    });
+    required this.color,
+    required this.showShadow,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -17,17 +22,34 @@ class MyButton extends StatelessWidget {
       onTap: onTap,
       child: Container(
         decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.secondary,
+          color: color,
           borderRadius: BorderRadius.circular(15),
           border: Border.all(
-            color: Theme.of(context).colorScheme.tertiary, // border color
-            width: 1,
+            color: const Color.fromARGB(255, 223, 223, 223), // border color
+            width: 1.5,
           ),
+          boxShadow: 
+          showShadow
+          ? [
+              BoxShadow(
+                color: Colors.black26,
+                blurRadius: 4,
+                offset: Offset(1, 2),
+              ),
+            ]
+          : [],
         ),
         
-        padding: EdgeInsets.all(25),
+        padding: EdgeInsets.all(20),
         margin: const EdgeInsets.symmetric(horizontal: 25),
-        child: Center(child: Text(text),),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: Colors.black
+            ),
+          ),
+        ),
       ),
     );
   }
