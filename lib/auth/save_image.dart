eced06a05761a75a1e868c9e6ffb3e21dab3f661
Voshiii <voshonfredison@gmail.com>
2025-09-17 22:37:13 +0200

    [Removed] save image, needs reimplementation

--- lib/auth/save_image.dart
+++ lib/auth/save_image.dart
@@ -1,27 +1,30 @@
 import 'dart:typed_data';
 import 'dart:convert';
 import 'package:flutter/services.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
+// import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-Future<void> saveImageToGallery(String base64String) async {
-  // Decode Base64 to Uint8List
-  Uint8List bytes = base64Decode(base64String);
+// ! TODO: THERE IS AN ISSUE WITH ImageGallerySaver, [save image not implemented]
 
-  // Request permission (iOS needs this)
-  var status = await Permission.photosAddOnly.request();
-  if (status.isGranted) {
-    final result = await ImageGallerySaver.saveImage(bytes);
-    if (result['isSuccess']) {
-      print("Image saved to gallery successfully!");
-    } else {
-      print("Failed to save image.");
-    }
-  } else {
-    print("Permission denied.");
-    await requestPermission();
-  }
-}
+// TODO work on saving images
+// Future<void> saveImageToGallery(String base64String) async {
+//   // Decode Base64 to Uint8List
+//   Uint8List bytes = base64Decode(base64String);
+
+//   // Request permission (iOS needs this)
+//   var status = await Permission.photosAddOnly.request();
+//   if (status.isGranted) {
+//     final result = await ImageGallerySaver.saveImage(bytes);
+//     if (result['isSuccess']) {
+//       print("Image saved to gallery successfully!");
+//     } else {
+//       print("Failed to save image.");
+//     }
+//   } else {
+//     print("Permission denied.");
+//     await requestPermission();
+//   }
+// }
 
 Future<void> requestPermission() async {
   PermissionStatus status = await Permission.photosAddOnly.request();
