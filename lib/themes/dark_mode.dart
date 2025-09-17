a07273e2699c340ad68283f48bc2f7cd00bd014d
Voshiii <voshonfredison@gmail.com>
2025-09-17 21:45:53 +0200

    [Changed] colors, NEED TO BE CHANGED BETTER

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 
+//! CHANGE THE COLORS ACCORDINGLY
 ThemeData darkMode = ThemeData(
   colorScheme: ColorScheme.dark(
     surface: Colors.grey.shade900,
-    primary: Colors.grey.shade600,
+    // primary: Colors.grey.shade600,
+    primary: Colors.blue,
     secondary: const Color.fromARGB(255, 57, 57, 57),
     tertiary: const Color.fromARGB(255, 172, 172, 172),
     inversePrimary: Colors.grey.shade300,
