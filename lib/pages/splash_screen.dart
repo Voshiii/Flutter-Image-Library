31ea1b66beca93190273b5b1a18ae46f3c490859
Voshiii <voshonfredison@gmail.com>
2025-09-18 00:18:10 +0200

    [Optimize] add JWT token when login

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:photo_album/auth/auth.dart';
+import 'package:photo_album/auth/dio.dart';
 import 'package:photo_album/auth/face_id_auth.dart';
 import 'package:photo_album/components/error_dialog.dart';
 import 'package:photo_album/components/push_homescreen.dart';
@@ -40,10 +41,11 @@ class SplashScreenState extends State<MySplashScreen> with SingleTickerProviderS
   Future<void> _loadDataAndLogin() async {
     final bool prefFaceID = await AuthService.getFaceIdPref();
     bool loggedIn = await AuthService.isLoggedIn();
+    final token = await AuthService.getToken();
 
     // print("Is logged in: $loggedIn");
 
-    if(!loggedIn){
+    if(!loggedIn || token == null){
       await AuthService.saveFaceIDPref(false);
 
       // if(!_removeSplashScreen){
@@ -66,14 +68,15 @@ class SplashScreenState extends State<MySplashScreen> with SingleTickerProviderS
     }
     else{
       // Navigate to homescreen if already logged in
-      _attemptLoginWithFaceID(prefFaceID);
+      _attemptLoginWithFaceID(prefFaceID, token);
     }
   }
 
-  void _attemptLoginWithFaceID(bool prefFaceID) async {
+  void _attemptLoginWithFaceID(bool prefFaceID, String token) async {
     if(prefFaceID){
       final didAuth = await authenticateUser();
       if(didAuth){
+        await Api.setAuthToken(token);
         if(!mounted) return;
         pushToHomeScreen(context, "Fade");
       }
@@ -90,7 +93,7 @@ class SplashScreenState extends State<MySplashScreen> with SingleTickerProviderS
                 child: Text("Ok"),
                 onPressed: () {
                   Navigator.pop(context);
-                  _attemptLoginWithFaceID(prefFaceID);
+                  _attemptLoginWithFaceID(prefFaceID, token);
                 },
               )
             ],
@@ -99,6 +102,7 @@ class SplashScreenState extends State<MySplashScreen> with SingleTickerProviderS
       }
     }
     else{
+      await Api.setAuthToken(token);
       if(!mounted) return;
       pushToHomeScreen(context, "Fade");
     }
